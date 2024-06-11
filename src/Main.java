ca6380ea27758c7bcd14c9ae2b82d03691d77a4b
Catarine Gonçalves <Catarine>
2024-06-11 14:46:29 -0300

    Manipulação de Strings e Datas

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String welcome;
+        String name = "Catarine";
+
+        LocalDate today = LocalDate.now();
+        //Locale brazil = new Locale("pt", "BR");
+        Locale brazil = Locale.of("pt", "BR");
+        String dayOfWeek = today.getDayOfWeek().getDisplayName(TextStyle.FULL, brazil);
+
+
+        LocalDateTime now = LocalDateTime.now();
+        int hourOfDay = now.getHour();
+
+        if (hourOfDay >= 0 && hourOfDay < 12) {
+            welcome = "Bom dia";
+        } else if (hourOfDay >= 12 && hourOfDay < 18) {
+            welcome = "Boa tarde";
+        } else {
+            welcome = "Boa noite";
         }
+
+        System.out.printf("%s %s! Hoje é %s.%n", welcome, name, dayOfWeek);
     }
 }
