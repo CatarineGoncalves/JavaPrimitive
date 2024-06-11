8fc62bc5c123084d9afaed50925e8ef1ea54386c
Catarine Gonçalves <Catarine>
2024-06-11 16:01:23 -0300

    funcoes no java

--- src/vetores.java
+++ src/vetores.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class vetores {
+import java.util.Arrays;
+
+public class vetores {
+    public static void main(String[] args) {
+
+        int[] numbers = {100, 12, 23, 48, 339, 123, 32, 55};
+        int menor = numbers[0];
+        int maior = numbers[0];
+        int media = 0;
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] > maior) {
+                maior = numbers[i];
+            }
+
+            if(numbers[i] < menor) {
+                menor = numbers[i];
+            }
+
+            media += numbers[i];
+
+        }
+
+        System.out.println("O menor número é " +menor+ ", já o maior número é " +maior+ ", e a média entre todos os números é de " +media/numbers.length);
+
+//
+//
+//        String[] letras = {"A", "B", "C", "D"};
+//
+//        for (int i = 0; i < letras.length ; i++) {
+//            System.out.println(Arrays.toString(letras));
+//        }
+
+
+
+    }
+
 }
